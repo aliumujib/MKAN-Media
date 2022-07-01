83aa9dc4d7e4d03b34392591075ed022bd967df2
Abdul-Mujeeb Aliu <aliuabdulmujib@yahoo.com>
2022-07-01 02:22:09 +0100

    Added redis config that uses env variables

--- config/db/redis.go
+++ config/db/redis.go
@@ -2,18 +2,18 @@ package dbs
 
 import (
 	"fmt"
+	"github.com/MKA-Nigeria/mkanmedia-go/config"
 	"time"
 
 	"github.com/go-redis/redis"
-	"github.com/spf13/viper"
 )
 
 //ConnectRedis returns a connection to posgress instance
 func ConnectRedis() *redis.Client {
-	env := viper.GetString("env")
+	url := config.Env.REDIS_URL
 	client := redis.NewClient(&redis.Options{
-		Addr:        viper.GetString(env + ".db.redis.host"),
-		Password:    viper.GetString(env + ".db.redis.password"),
+		Addr:        url,
+		Password:    "",
 		DialTimeout: time.Second * 20,
 		DB:          0,
 	})
--- config/env.go
+++ config/env.go
@@ -8,6 +8,7 @@ type env struct {
 	ENV                       string
 	PORT                      string
 	MONGO_URL                 string
+	REDIS_URL                 string
 	SOUND_CLOUD_CLIENT_ID     string
 	SOUND_CLOUD_CLIENT_SECRET string
 }
@@ -17,6 +18,7 @@ func (e env) Validate() error {
 		validation.Field(&e.ENV, validation.Required),
 		validation.Field(&e.PORT, validation.Required),
 		validation.Field(&e.MONGO_URL, validation.Required),
+		validation.Field(&e.REDIS_URL, validation.Required),
 		validation.Field(&e.SOUND_CLOUD_CLIENT_ID, validation.Required),
 		validation.Field(&e.SOUND_CLOUD_CLIENT_SECRET, validation.Required),
 	)
